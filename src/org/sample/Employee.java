91626988fad8680270c96041b3b881c277442f02
avkkumar1998 <avkkumar1998@gmail.com>
2022-08-25 20:47:06 +0530

    added

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class Employee {
+	private void idName() {
+		
+		System.out.println("ID Name is Krishna");
+
+	}
+public static void main(String[] args) {
+	Employee e=new Employee();
+	e.idName();
+}
+}
